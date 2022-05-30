d138f1ddccdaeada88e49cd4d205208101bd6628
yooonyng <yooonyng@gmail.com>
2022-05-30 10:16:47 +0900

    0530

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # streamlit-juvenile-crime
 
-소년범죄
+소년 범죄자의 주변 환경과 범죄 처분 결과 상황을 파악
--- app.py
+++ app.py
@@ -1,8 +1,31 @@
 import streamlit as st
-import pandas as pd
+import pandas as pd 
+import numpy as np 
+import random
+import seaborn as sb
+from fbprophet import Prophet
+import matplotlib.pyplot as plt
 
 def main():
-    pass
+    st.title('소년범죄')
+
+    menu = ['상위 범죄순위','부모관계','교육정도','정신상태','전회처분 상황','처분결과 상황','재범기간']
+    choice = st.sidebar.selectbox('메뉴 선택',menu)
+
+    if choice == menu[0]:
+        pass
+    elif choice == menu[1]:
+        pass
+    elif choice == menu[2]:
+        pass
+    elif choice == menu[3]:
+        pass
+    elif choice == menu[4]:
+        pass
+    elif choice == menu[5]:
+        pass
+    elif choice == menu[6]:
+        pass
 
 if __name__ == '__main__':
     main()
