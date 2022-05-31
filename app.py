02ea41aa868bd2d1e3a20a96e0996b2598fd32fd
yooonyng <yooonyng@gmail.com>
2022-05-31 12:29:32 +0900

    0531

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from app_top5 import run_top5
 from app_parents import run_parents
 from app_study import run_study
 from app_mental import run_mental
+from app_result1 import run_result1
+from app_result2 import run_result2
+from app_again import run_again
 
 
 def main():
@@ -23,11 +26,11 @@ def main():
     elif choice == menu[4]:
         run_mental()
     elif choice == menu[5]:
-        pass
+        run_result1()
     elif choice == menu[6]:
-        pass
+        run_result2()
     elif choice == menu[7]:
-        pass
+        run_again()
 
 if __name__ == '__main__':
     main()
