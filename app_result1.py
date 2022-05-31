5e46d6b9c1d1316febcfdb3dc8ba24255e7a205a
yooonyng <yooonyng@gmail.com>
2022-05-31 15:44:34 +0900

    0531

--- app_result1.py
+++ app_result1.py
@@ -40,8 +40,7 @@ def run_result1():
     st.subheader('전회처분 상황')
 
     fig1 = px.pie(df_re1_5_sum,names=['초범', '기소유예', '선도유예', '보호처분', '형(재산형포함)집행종료'],
-        values=[35477, 7632, 1389, 12016, 2885],
-        title='소년범죄 전회처분 상황')
+        values=[35477, 7632, 1389, 12016, 2885])
     st.plotly_chart(fig1)
 
 
